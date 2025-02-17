d59d4c01cf78ecdca83c4f45b927969702b706b4
AwaisKhan50 <awaiskhan.252580@gmail.com>
2025-02-17 22:36:48 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+
+// import Card from './components/Card'
+// import ChangeColProject from './components/ChangeColProject'
+import PasswordGene from './components/PasswordGene'
+
+const App = () => {
+
+  const data=[
+    {
+      "image": "https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
+      "title": "John Doe",
+      "description": "Software Engineer with 5+ years of experience in web development and cloud computing."
+    },
+    {
+      "image": "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=800",
+      "title": "Jane Smith",
+      "description": "Graphic Designer specializing in branding and digital illustration. Passionate about minimalist design."
+    },
+    {
+      "image": "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
+      "title": "Alex Johnson",
+      "description": "Data Scientist with expertise in machine learning and big data analytics. Loves solving complex problems."
+    },
+    {
+      "image": "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=800",
+      "title": "Emily Brown",
+      "description": "Marketing Strategist with a focus on digital marketing and social media campaigns. Always up-to-date with trends."
+    },
+    {
+      "image": "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
+      "title": "Michael Lee",
+      "description": "Product Manager with a strong background in agile methodologies and user-centered design."
+    }
+  ]
+
+  return (
+      <PasswordGene/>
+  )
+}
+
+export default App
---
+++ src/components/PasswordGene.jsx
@@ -0,0 +1,69 @@
+import React, { useCallback, useEffect, useRef, useState } from 'react'
+
+const PasswordGene = () => {
+
+    const [length, setLength] = useState(8);
+    const [numberAllowed, setNumberAllowed] = useState(false);
+    const [charAllowed, setCharAllowed] = useState(false);
+    const [password, setPassword] = useState("");
+  const passwordRef = useRef(null);
+    const generatePassword = useCallback(() => {
+        let pass = "";
+        let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        if (numberAllowed) str += "0123456789";
+        if (charAllowed) str += "!@#$%&}{*+";
+        for (let i = 0; i < length; i++) {
+            let char = Math.floor(Math.random() * str.length);
+            pass += str.charAt(char);
+        }
+        setPassword(pass);
+    }, [length, numberAllowed, charAllowed]);
+
+    const copyText=useCallback(()=>{
+
+      passwordRef.current?.select();
+     window.navigator.clipboard.writeText(password);
+    },[password])
+      
+
+    useEffect(() => {
+        generatePassword();
+    }, [length, numberAllowed, charAllowed, generatePassword]);
+
+    return (
+        <div className='flex flex-col items-center justify-center h-screen bg-gradient-to-r from-purple-500  to-indigo-500'>
+            <div className='flex flex-col justify-center h-auto shadow-2xl bg-gray-300 shadow-black w-[40%] mt-10 p-6 rounded-lg '>
+                <h1 className='text-2xl font-bold text-center mb-4 text-gray-800'>Password Generator</h1>
+                <div className='flex gap-5 w-full mb-4'>
+                    <input className=' w-full rounded border-none outline-none bg-gray-100 h-10 p-2 text-gray-800' type="text" value={password} ref={passwordRef} readOnly />
+                    <button onClick={copyText} className='bg-blue-600 text-white px-4 py-2 rounded cursor-pointer  hover:bg-blue-700 transition duration-300 '>Copy</button>
+                </div>
+                <div className='flex gap-4 mt-8 items-center'>
+                    <input
+                        type="range"
+                        min={6}
+                        max={100}
+                        value={length}
+                        className='cursor-pointer w-full'
+                        onChange={(e) => { setLength(e.target.value) }}
+                    />
+                    <label className='text-gray-800'>Length: {length}</label>
+                </div>
+                <div className='flex gap-4 mt-4 items-center'>
+                    <input type="checkbox" id='numberInput' checked={numberAllowed}
+                        onChange={() => { setNumberAllowed((prev) => !prev) }}
+                    />
+                    <label className='text-gray-800' htmlFor='numberInput'>Include Numbers</label>
+                </div>
+                <div className='flex gap-4 mt-4 items-center'>
+                    <input type="checkbox" id='characterInput' checked={charAllowed}
+                        onChange={() => { setCharAllowed((prev) => !prev) }}
+                    />
+                    <label htmlFor='characterInput' className='text-gray-800'>Include Special Characters</label>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default PasswordGene
